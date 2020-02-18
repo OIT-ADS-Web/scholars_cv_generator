1464a1bdcfc237e4f038f41e35c47f0504966f5d
Harry Thakkar <ht29@duke.edu>
2020-02-18 10:36:18 -0500

    Revert "changes for fix"
    This reverts commit b2f5f162296c096dfc7122522b9c36cb0ad56789.

--- src/widgets_som_parser.js
+++ src/widgets_som_parser.js
@@ -339,7 +339,7 @@ class WidgetsSOMParser {
 
   parseConsultantAppointments(data) {
 
-    var consultAppointmentsList = { 'community_service': [], 'editorial_activities': [], 'prof_editorial_activities': [], 'professional_development': [], 'lectures': [], 'consulting': [], 'prof_consulting': [], 'other_activities': [], 'event_admin': [], 'committee_service': [], 'full_org_and_participation_list': [] };
+    var consultAppointmentsList = { 'community_service': [], 'editorial_activities': [], 'prof_editorial_activities': [], 'professional_development': [], 'lectures': [], 'consulting': [], 'prof_consulting': [], 'other_activities': [], 'event_admin': [], 'committee_service': [] };
     let professionalActivities = data['professionalActivities'];
 
      _.forEach(professionalActivities, function(value) {
@@ -356,7 +356,6 @@ class WidgetsSOMParser {
 
                 case "Community Service": {
                     consultAppointmentsList['community_service'].push({'label':label, 'date':startDate});
-                    consultAppointmentsList['full_org_and_participation_list'].push({'label':label, 'date':startDate});
                     break;
                 }
 
@@ -370,13 +369,11 @@ class WidgetsSOMParser {
 
                 case "Professional Development": {
                     consultAppointmentsList['professional_development'].push({'label':label, 'date':startDate});
-                    consultAppointmentsList['full_org_and_participation_list'].push({'label':label, 'date':startDate});
                     break;
                 }
 
                 case "Lecture": {
                     consultAppointmentsList['lectures'].push({'label':label, 'date':startDate});
-                    consultAppointmentsList['full_org_and_participation_list'].push({'label':label, 'date':startDate});
                     break;
                 }
 
@@ -396,19 +393,16 @@ class WidgetsSOMParser {
 
                 case "Other": {
                     consultAppointmentsList['other_activities'].push({'label':label, 'date':startDate});
-                    consultAppointmentsList['full_org_and_participation_list'].push({'label':label, 'date':startDate});
                     break;
                 }
 
                 case "Event/Organization Administration": {
                     consultAppointmentsList['event_admin'].push({'label':label, 'date':startDate});
-                    consultAppointmentsList['full_org_and_participation_list'].push({'label':label, 'date':startDate});
                     break;
                 }
 
                 case "Committee Service": {
                     consultAppointmentsList['committee_service'].push({'label':label, 'date':startDate});
-                    consultAppointmentsList['full_org_and_participation_list'].push({'label':label, 'date':startDate});
                     break;
                 }
 
