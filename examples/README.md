d2614f23cc526985e488be8f78a686572cb5312c
Rob Nelson <robert.nelson@duke.edu>
2020-11-06 08:36:47 -0500

    add examples so api results can be reviewed outside of call to widgets

---
+++ examples/README.md
@@ -0,0 +1,9 @@
+these are just examples of the api of the application
+
+To run you have to use this command:
+
+> babel-node examples/<example_*>.js 
+
+(must npm install babel-cli --global)
+
+
---
+++ examples/example_publications.js
@@ -0,0 +1,27 @@
+// NOTE: to run > babel-node examples/example_publications.js
+// need .env file at root
+require('dotenv').config();
+const widgetsUrl = process.env.WIDGETS_URL
+console.log("WIDGETS_URL="+widgetsUrl)
+
+import * as data from './per8345372_test.json'
+import { WidgetsPubMedParser } from '../src/widgets_pub_med_parser'
+
+let widgetsParser = new WidgetsPubMedParser()
+
+let publications = widgetsParser.parsePublications(data)
+let keys = Object.keys(publications)
+// can be -> 'journals', 'manuscripts', 'letters', 'editorials',
+// 'abstracts', 'reviews', 'others', 'nonauthored', 'books',
+// 'booksections'
+for (var key of keys) {
+    let obj = publications[key]
+    if (Array.isArray(obj)) {
+        console.log(`****${key}:${obj.length}*****`)
+        obj.forEach(publication => {
+            console.log(publication)
+        })
+    } else {
+       console.log(key + " empty object")
+    }
+}
