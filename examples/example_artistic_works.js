ac09dea34637c87731f6e00492c3fbc47d129908
Rob Nelson <robert.nelson@duke.edu>
2022-08-19 14:48:25 -0400

    FDP-3868: take into account 'date-precision' when generating text for artistic-work

---
+++ examples/example_artistic_works.js
@@ -0,0 +1,15 @@
+// NOTE: to run > babel-node examples/example_publications.js 
+// need .env file at root
+require('dotenv').config();
+const widgetsUrl = process.env.WIDGETS_URL
+console.log("WIDGETS_URL="+widgetsUrl)
+
+import * as data from './per0482202_test.json'
+import { WidgetsParser } from '../src/widgets_parser'
+
+let widgetsParser = new WidgetsParser()
+
+let works = widgetsParser.parseArtisticWorks(data)
+let events = widgetsParser.parseArtisticEvents(data)
+let details = {...works, ...events }
+console.log(details)
